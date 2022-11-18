900e6a54bd3826f3e0a76e3fa6800329719163ca
Alliques <airat1100@bk.ru>
2022-11-18 12:14:36 +0300

    home page. first section

--- src/Components/HomeFirstSection.tsx
+++ src/Components/HomeFirstSection.tsx
@@ -1,5 +1,5 @@
 import s from "./HomeFirstSection.module.css";
-
+import cl from "classnames";
 export const HomeFirstSection = () => {
   return (
     <section className={s["greeting-section"]}>
@@ -8,6 +8,12 @@ export const HomeFirstSection = () => {
         <div className={s.subtitle}>
           Designer furniture and decore made
           <br /> of wood and epoxy resin.
+        </div>
+        <div className={s.navigation_btns}>
+          <button className={cl(s.navigation_btn, s.catalog_btn)}>
+            OPEN CATALOG
+          </button>
+          <button className={s.navigation_btn}>DESIGN YOUR TABLE</button>
         </div>
       </div>
     </section>
