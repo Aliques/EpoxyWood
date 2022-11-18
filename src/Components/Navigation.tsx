74c9a4910fe30591678899c610a5c50591109f37
Alliques <airat1100@bk.ru>
2022-11-18 21:46:05 +0300

    Navbar opacity on Scroll

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -5,60 +5,70 @@ import user from "../Icons/24/user.svg";
 import SearchField from "./Controls/SearchField";
 import s from "./Navigation.module.css";
 import cl from "classnames";
+import { useEffect, useRef } from "react";
 export const Navigation = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  window.addEventListener("scroll", function () {
+    if (ref.current != null) {
+      ref.current.style.opacity = this.window.scrollY < 50 ? "0" : "1";
+    }
+  });
   return (
-    <nav className={s.navbar}>
-      <div className={s["navbar-container"]}>
-        <div className={cl(s["navigation-item"], s.logo)}>
-          EPOXY
-          <br />
-          HOUSE
-        </div>
-        <div className={s["navbar-links__container"]}>
-          <div className={s["navbar-links"]}>
-            <input type="checkbox" />
-            <div className={s["hamburger-lines"]}>
-              <span className={cl(s.line, s.line1)}></span>
-              <span className={cl(s.line, s.line2)}></span>
-              <span className={cl(s.line, s.line3)}></span>
-            </div>
-            <div className={s.cover}></div>
+    <div>
+      <div className={s.nav_bg} ref={ref}></div>
+      <nav className={s.navbar}>
+        <div className={s["navbar-container"]}>
+          <div className={cl(s["navigation-item"], s.logo)}>
+            EPOXY
+            <br />
+            HOUSE
+          </div>
+          <div className={s["navbar-links__container"]}>
+            <div className={s["navbar-links"]}>
+              <input type="checkbox" />
+              <div className={s["hamburger-lines"]}>
+                <span className={cl(s.line, s.line1)}></span>
+                <span className={cl(s.line, s.line2)}></span>
+                <span className={cl(s.line, s.line3)}></span>
+              </div>
+              <div className={s.cover}></div>
 
-            <ul className={s["menu-items"]}>
-              <li>
-                <a href="#">CATALOG</a>
-              </li>
-              <li>
-                <a href="#">FAQ</a>
-              </li>
-              <li>
-                <a href="#">CONTACTS</a>
-              </li>
-            </ul>
-          </div>
-          <div className={s.search}>
-            <SearchField />
-          </div>
-          <div className={s["nav-icon__container"]}>
-            <div>
-              <div className={s["navigation-icons"]}>
-                <img src={globe} />
-              </div>
-              <div className={s["navigation-icons"]}>
-                <img src={user} />
-              </div>
+              <ul className={s["menu-items"]}>
+                <li>
+                  <a href="#">CATALOG</a>
+                </li>
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">CONTACTS</a>
+                </li>
+              </ul>
             </div>
-            <div>
-              <div className={s["navigation-icons"]}>
-                <img src={scales} />
+            <div className={s.search}>
+              <SearchField />
+            </div>
+            <div className={s["nav-icon__container"]}>
+              <div>
+                <div className={s["navigation-icons"]}>
+                  <img src={globe} />
+                </div>
+                <div className={s["navigation-icons"]}>
+                  <img src={user} />
+                </div>
               </div>
-              <div className={s["navigation-icons"]}>
-                <img src={cart} />
+              <div>
+                <div className={s["navigation-icons"]}>
+                  <img src={scales} />
+                </div>
+                <div className={s["navigation-icons"]}>
+                  <img src={cart} />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
