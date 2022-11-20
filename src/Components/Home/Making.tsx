ea039cc06493834dc7a61d0c331b55fdc0a9deb9
Alliques <airat1100@bk.ru>
2022-11-20 23:21:25 +0300

    how to make section

---
+++ src/Components/Home/Making.tsx
@@ -0,0 +1,66 @@
+import s from "./Making.module.css";
+import cl from "classnames";
+import RoundedButton from "../Controls/RoundedButton";
+import firstImg from "../../Temp/MakingDescriptionImg_1.jpg";
+export const Making = () => {
+  return (
+    <div>
+      <div className={s.title}>How to make an order</div>
+      <div className={s.makeing_container}>
+        <div className={s.description_container}>
+          <div className={s.description_text}>
+            <div className={s.decription_point}>
+              <span className={s.number}>1</span>
+              <div>
+                Disscussion of design;
+                <br />
+                payment 30%;
+                <br />
+                selection of wood, resin color, legs and coating.
+              </div>
+            </div>
+            <div className={s.decription_point}>
+              <div className={s.number}>2</div>
+              <div>
+                We will make an approximate calculation of the cost of the order
+                based on your details and taking into account your wishes.
+              </div>
+            </div>
+            <div className={s.decription_point}>
+              <div className={s.number}>3</div>
+              <div>
+                We conclude a contract with you to assure you of our
+                profesionalism.
+              </div>
+            </div>
+          </div>
+          <img src={firstImg} alt="#" />
+        </div>
+        <div className={s.description_container}>
+          <img src={firstImg} alt="#" />
+          <div className={s.description_text}>
+            <div className={s.decription_point}>
+              <div className={s.number}>4</div>
+              <div>
+                For the product to be made, we select the wood, which then
+                passes through various stages of preparation and procesing.
+              </div>
+            </div>
+            <div className={s.decription_point}>
+              <div className={s.number}>5</div>
+              <div>
+                The final stage is transportation and payment for the work
+                performed under the contract.
+              </div>
+            </div>
+            <div className={s.decription_point}>
+              <RoundedButton width={160} height={40}>
+                Design your table
+              </RoundedButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,16 +1,18 @@
 import { AboutUs } from "../Components/Home/AboutUs";
 import { HomeFirstSection } from "../Components/Home/HomeFirstSection";
+import { Making } from "../Components/Home/Making";
 import { NewProducts } from "../Components/Home/NewProducts";
 import { ProductTypesSection } from "../Components/Home/ProductTypesSection";
 import s from "./Home.module.css";
 
 export const Home = () => {
   return (
-    <div>
+    <div style={{ paddingBottom: 50 }}>
       <HomeFirstSection />
       <ProductTypesSection />
       <NewProducts />
-      <AboutUs></AboutUs>
+      <AboutUs />
+      <Making />
     </div>
   );
 };
