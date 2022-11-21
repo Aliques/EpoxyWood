c71a325730bd1e9291e671070a29b1bd05872087
Alliques <airat1100@bk.ru>
2022-11-21 12:35:46 +0300

    clients design tables section

--- src/Components/Home/ProductsPreviewTile.tsx
+++ src/Components/Home/ProductsPreviewTile.tsx
@@ -1,13 +1,11 @@
 /**/
-import s from "./NewProducts.module.css";
+import s from "./ProductsPreviewTile.module.css";
 import cl from "classnames";
-import material from "../../public/Images/MaterialOverview.png";
 import { ProductCard } from "../Controls/ProductCard";
 import RoundedButton from "../Controls/RoundedButton";
-export const NewProducts = () => {
+export const ProductsPreviewTile = () => {
   return (
     <div className={s.container}>
-      <div className={s.title}>New products</div>
       <div className={s.cards_container}>
         <div className={s.card_block}>
           <ProductCard />
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,27 @@
 import { AboutUs } from "../Components/Home/AboutUs";
 import { HomeFirstSection } from "../Components/Home/HomeFirstSection";
 import { Making } from "../Components/Home/Making";
-import { NewProducts } from "../Components/Home/NewProducts";
+import { ProductsPreviewTile } from "../Components/Home/ProductsPreviewTile";
 import { ProductTypesSection } from "../Components/Home/ProductTypesSection";
 import s from "./Home.module.css";
-
+import sl from "classnames";
 export const Home = () => {
   return (
     <div style={{ paddingBottom: 50 }}>
       <HomeFirstSection />
       <ProductTypesSection />
-      <NewProducts />
+      <div>
+        <div className={sl(s.section_title, s.products)}>New products</div>
+        <ProductsPreviewTile />
+      </div>
       <AboutUs />
       <Making />
+      <div>
+        <div className={sl(s.section_title, s.clients_designes)}>
+          Client's table design
+        </div>
+        <ProductsPreviewTile />
+      </div>
     </div>
   );
 };
