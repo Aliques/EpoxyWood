c54f2bea0f93bf59c8b8c22915d5d10c984d8d0c
Aliques <ayrat.mt@gmail.com>
2022-12-24 19:04:55 +0300

    product photo preview layout

--- src/Components/Product/DescriptionMediaDesktop.tsx
+++ src/Components/Product/DescriptionMediaDesktop.tsx
@@ -9,7 +9,7 @@ export const DescriptionMediaDesktop = () => {
     let images: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     return (
         <div className={s.container}>
-
+            
         </div>
     );
 };
--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -1,13 +1,13 @@
 import s from "./Product.module.css";
 import sl from "classnames";
 import { useParams } from "react-router-dom";
-import { DescriptionTabs } from "../Components/Product/DescriptionTabs";
+import { TabsDesktop } from "../Components/Product/TabsDesktop";
 export const Product = () => {
   const { id } = useParams();
   return (
     <div className={s.container}>
       <div className={s.description_container}>
-        <DescriptionTabs />
+        <TabsDesktop />
       </div>
       <div className={s.order_container}></div>
     </div>
